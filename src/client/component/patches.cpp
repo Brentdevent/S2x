36f3dd6c6233321e571ef95493fc9c78742be761
Brent van Lieshout <70229620+Brentdevent@users.noreply.github.com>
2026-06-04 22:51:09 +0200

    Allow multiple instances for Debug builds

---
+++ src/client/component/patches.cpp
@@ -0,0 +1,28 @@
+#include <std_include.hpp>
+#include "loader/component_loader.hpp"
+
+#include "game/game.hpp"
+
+#include <utils/hook.hpp>
+
+namespace patches
+{
+	namespace
+	{
+		
+	}
+
+	class component final : public multiplayer_component
+	{
+	public:
+		void post_thread_setup() override
+		{
+#ifdef DEBUG
+			// Allow multiple instances for testing purposes.
+			utils::hook::set(0x78A5F0_g, 0xC301B0);
+#endif
+		}
+	};
+}
+
+REGISTER_COMPONENT(patches::component)
