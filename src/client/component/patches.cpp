912365b1b1336cf21c2a146a5f3d76995dff9dd8
Brent van Lieshout <70229620+Brentdevent@users.noreply.github.com>
2026-08-18 22:18:27 +0200

    Emulate bdPublisherVariables responses
    Fixes #6 and many other things that got disabled without this.

--- src/client/component/patches.cpp
+++ src/client/component/patches.cpp
@@ -9,50 +9,6 @@
 
 namespace patches
 {
-	namespace
-	{
-		utils::hook::detour clear_hub_vendor_targets_hook;
-
-		void register_hub_vendors()
-		{
-			game::CG_RegisterHubVendorTarget(415, "quartermaster_menu");
-			game::CG_RegisterHubVendorTarget(413, "headquarters_menu");
-			game::CG_RegisterHubVendorTarget(377, "blacksmith_menu");
-			game::CG_RegisterHubVendorTarget(427, "mail_officer_menu");
-			game::CG_RegisterHubVendorTarget(414, "zombie_officer_menu");
-			game::CG_RegisterHubVendorTarget(401, "general_menu");
-			game::CG_RegisterHubVendorTarget(407, "onevone_officer_menu");
-			game::CG_RegisterHubVendorTarget(510, "theater_menu");
-			game::CG_RegisterHubVendorTarget(396, "scorestreak_officer_menu");
-			game::CG_RegisterHubVendorTarget(426, "company_commander_menu");
-			game::CG_RegisterHubVendorTarget(369, "tdm_playlist_menu");
-			game::CG_RegisterHubVendorTarget(368, "dom_playlist_menu");
-			game::CG_RegisterHubVendorTarget(370, "war_playlist_menu");
-			game::CG_RegisterHubVendorTarget(397, "r_and_r_menu");
-			game::CG_RegisterHubVendorTarget(402, "overlook_menu");
-			game::CG_RegisterHubVendorTarget(403, "division_prestige_menu");
-
-			console::info("[hub] registered vendor targets\n");
-		}
-
-		bool is_full_hub()
-		{
-			const auto* mapname = game::Dvar_FindMalleableVar("mapname");
-			return mapname && mapname->current.string &&
-				   mapname->current.string == "mp_hub_allies"s;
-		}
-
-		void clear_hub_vendor_targets_stub()
-		{
-			clear_hub_vendor_targets_hook.invoke();
-
-			if (is_full_hub())
-			{
-				register_hub_vendors();
-			}
-		}
-	}
-
 	class component final : public multiplayer_component
 	{
 	public:
@@ -63,24 +19,9 @@ namespace patches
 		}
 
 		void post_unpack() override
-		{
-			// Temp fix for vendor interactions in the Hub 
-			// Normally this seems to get set from GSC, but it doesn't for some reason
-			clear_hub_vendor_targets_hook.create(0x4D9D0_g, clear_hub_vendor_targets_stub);
-
-			// Show HQ tab in menu
-			game::Dvar_RegisterBoolProtected("allow_hub_vendor_menu", true, game::DVAR_FLAG_NONE);
-			// Alternate hub flow
-			game::Dvar_RegisterBoolProtected("2917", true, game::DVAR_FLAG_NONE); 
-			// Full hub flow false
-			game::Dvar_RegisterBool("2464", false, game::DVAR_FLAG_NONE);         
-			// vlobby enabled
-			game::Dvar_RegisterBool("4287", true, game::DVAR_FLAG_NONE);          
+		{          
 			// Skip intro's
 			game::Dvar_RegisterBool("2665", true, game::DVAR_FLAG_NONE);   
-
-			// Enable the stock AAR winners-circle flow
-			game::Dvar_RegisterBool("5320", true, game::DVAR_FLAG_READ);
 		}
 	};
 }
--- src/client/game/demonware/services/bdPublisherVariables.cpp
+++ src/client/game/demonware/services/bdPublisherVariables.cpp
@@ -1,17 +1,84 @@
 #include <std_include.hpp>
 #include "../dw_include.hpp"
 
+#include "component/console/console.hpp"
+
+#include <utils/nt.hpp>
+
+#include "resource.hpp"
+
 namespace demonware
 {
+	namespace
+	{
+		struct publisher_variables_resource
+		{
+			std::string_view name_space;
+			std::uint16_t major_version;
+			std::uint16_t minor_version;
+			int resource_id;
+		};
+
+		constexpr publisher_variables_resource publisher_variables[]
+		{
+			{"mp_tu26", 1, 20, DW_PUBLISHER_VARIABLES_MP},
+			{"mm_game_tu26", 1, 12, DW_PUBLISHER_VARIABLES_MM_GAME},
+			{"mm_hub_tu26", 1, 12, DW_PUBLISHER_VARIABLES_MM_HUB},
+			{"mm_party_tu26", 1, 11, DW_PUBLISHER_VARIABLES_MM_PARTY},
+			{"qos_tu26", 1, 14, DW_PUBLISHER_VARIABLES_QOS},
+		};
+	}
+
 	bdPublisherVariables::bdPublisherVariables() : service(95, "bdPublisherVariables")
 	{
 		this->register_task(1, &bdPublisherVariables::retrievePublisherVariables);
 	}
 
-	void bdPublisherVariables::retrievePublisherVariables(service_server* server, byte_buffer* /*buffer*/) const
+	void bdPublisherVariables::retrievePublisherVariables(service_server* server, byte_buffer* buffer) const
 	{
-		// TODO:
+		std::string context;
+		std::string name_space;
+		if (!buffer->read_string(&context) || !buffer->read_string(&name_space))
+		{
+			server->create_reply(this->task_id(), BD_PUBLISHER_VARIABLES_SERVICE_ERROR).send();
+			return;
+		}
+
+		console::demonware("[DW]: [bdPublisherVariables]: retrieving %s/%s\n",
+			context.c_str(), name_space.c_str());
+
+		const auto entry = std::ranges::find(publisher_variables, name_space,
+			&publisher_variables_resource::name_space);
+		if (entry == std::end(publisher_variables))
+		{
+			console::demonware("[DW]: [bdPublisherVariables]: unknown namespace: %s\n",
+				name_space.c_str());
+			server->create_reply(this->task_id(), BD_PUBLISHER_VARIABLES_INVALID_NAMESPACE).send();
+			return;
+		}
+
+		auto variables = utils::nt::load_resource(entry->resource_id);
+		while (!variables.empty() && (variables.back() == '\r' || variables.back() == '\n'))
+		{
+			variables.pop_back();
+		}
+
+		if (variables.empty())
+		{
+			console::demonware("[DW]: [bdPublisherVariables]: missing data for namespace: %s\n",
+				name_space.c_str());
+			server->create_reply(this->task_id(), BD_PUBLISHER_VARIABLES_SERVICE_ERROR).send();
+			return;
+		}
+
+		auto result = std::make_unique<bdPublisherVariablesInfo>();
+		result->majorVersion = entry->major_version;
+		result->minorVersion = entry->minor_version;
+		result->nameSpace = name_space;
+		result->variables = std::move(variables);
+
 		auto reply = server->create_reply(this->task_id());
+		reply.add(result);
 		reply.send();
 	}
 }
